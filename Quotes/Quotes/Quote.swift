39e2251cb6eb9ffdc23f694a472f36d2c8afeba0
Kitwana Akil <kakil@itelinc.com>
2016-02-17 08:56:11 -0500

    Begin implementing the Quote ViewController.

---
+++ Quotes/Quotes/Quote.swift
@@ -0,0 +1,32 @@
+//
+//  Quote.swift
+//  Quotes
+//
+//  Created by Kitwana on 2/17/16.
+//  Copyright © 2016 AkilDev. All rights reserved.
+//
+
+import Foundation
+
+struct Quote {
+    let text: String
+    let author: String
+    
+    static let all: [Quote] = [
+        Quote(text: "Never put off until tomorrow what you can do the day after tomorrow.", author: "Mark Twain"),
+        Quote(text: "Efficiency is doing better what is already being done.", author: "Peter Drucker"),
+        Quote(text: "To infinity and beyond!", author: "Buzz Lightyear"),
+        Quote(text: "May the Force be with you.", author: "Han Solo"),
+        Quote(text: "Simplicity is the ultimate sophistication", author: "Leonardo da Vinci"),
+        Quote(text: "It’s not just what it looks like and feels like. Design is how it works.", author: "Steve Jobs")
+    ]
+}
+
+
+// MARK: - Printable
+
+extension Quote: CustomStringConvertible {
+    var description: String {
+        return "\"\(text)\" - \(author)"
+    }
+}
